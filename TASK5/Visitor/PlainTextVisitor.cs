420f215a6ad695a0c623e750a516b0fb35d862b4
Кукіль Костянтин Павлович <ipz241_kkp@student.ztu.edu.ua>
2026-05-09 17:36:01 +0300

    add Visitor pattern for stats, search, serialization

---
+++ TASK5/Visitor/PlainTextVisitor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TASK5.Visitor
+{
+    class PlainTextVisitor : ILightNodeVisitor
+    {
+        private readonly StringBuilder _sb = new();
+        public string Result => _sb.ToString().Trim();
+
+        public void Visit(LightElementNode node)
+        {
+            foreach (var child in node.Children)
+                if (child is LightElementNode e) Visit(e);
+                else if (child is LightTextNode t) Visit(t);
+        }
+
+        public void Visit(LightTextNode node) =>
+            _sb.Append(node.OuterHTML().Trim() + " ");
+    }
+}
---
+++ TASK5/Visitor/StatsVisitor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TASK5.Visitor
+{
+    class StatsVisitor : ILightNodeVisitor
+    {
+        public int ElementCount { get; private set; }
+        public int TextCount { get; private set; }
+        public int MaxDepth { get; private set; }
+        private int _currentDepth;
+
+        public void Visit(LightElementNode node)
+        {
+            ElementCount++;
+            _currentDepth++;
+            if (_currentDepth > MaxDepth) MaxDepth = _currentDepth;
+
+            foreach (var child in node.Children)
+                if (child is LightElementNode e) Visit(e);
+                else if (child is LightTextNode t) Visit(t);
+
+            _currentDepth--;
+        }
+
+        public void Visit(LightTextNode node) => TextCount++;
+
+        public void PrintReport()
+        {
+            System.Console.WriteLine($"  Елементів:            {ElementCount}");
+            System.Console.WriteLine($"  Текстових вузлів:     {TextCount}");
+            System.Console.WriteLine($"  Максимальна глибина:  {MaxDepth}");
+        }
+    }
+}
