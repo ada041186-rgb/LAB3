8e6d4091db26f02a88baadecd3f88dd19a2b490d
Кукіль Костянтин Павлович <ipz241_kkp@student.ztu.edu.ua>
2026-05-09 17:51:17 +0300

    add Bridge pattern for multiple renderers

---
+++ TASK5/Renderer/IRenderer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TASK5.Renderer
+{
+    interface IRenderer
+    {
+        string RenderElement(string tagName, IReadOnlyList<string> cssClasses,
+                             IReadOnlyList<LightNode> children, int indent);
+        string RenderText(string text, int indent);
+    }
+}
---
+++ TASK5/Renderer/RenderedNode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TASK5.Renderer
+{
+    class RenderedNode
+    {
+        private readonly LightElementNode _node;
+        private IRenderer _renderer;
+
+        public RenderedNode(LightElementNode node, IRenderer renderer)
+        {
+            _node = node;
+            _renderer = renderer;
+        }
+
+        public void SetRenderer(IRenderer renderer) => _renderer = renderer;
+
+        public string Render() =>
+            _renderer.RenderElement(
+                _node.TagName,
+                _node.CssClasses,
+                _node.Children,
+                0);
+    }
+}
